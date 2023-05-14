ffe3a6855c0d7de7d441db4b2014830e36d261ec
midokhaled70 <107317230+midokhaled70@users.noreply.github.com>
2023-05-14 23:47:12 +0300

    Add files via upload

---
+++ js/Login.js
@@ -0,0 +1,25 @@
+function redirectme(url){
+	window.location.href=url;
+}
+const login_btn=document.getElementById("login-btn");
+
+
+// login_btn.addEventListener('click',()=>{
+//     redirectme('index.html');
+//     console.log("ajjj");
+
+// });
+
+
+
+
+
+
+
+// /const register_btn=document.getElementById("register-btn");
+
+// register_btn.addEventListener('click',()=>{
+//     redirectme('Login.html');
+//     console.log("ajjj");
+
+// })
